2e3d895b1a6e163f530d0d1da16f8eb405c6277b
Kim Dong Min <kkimniu@gmail.com>
2025-05-20 14:57:03 +0900

    초기 커밋

---
+++ src/main/java/com/example/mock_invest/MockInvestApplication.java
@@ -0,0 +1,13 @@
+package com.example.mock_invest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MockInvestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MockInvestApplication.class, args);
+	}
+
+}

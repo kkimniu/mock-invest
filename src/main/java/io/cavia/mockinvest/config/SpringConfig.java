ab0cfc63b3cd1ea21ec0ff1788e0d0d1f8465275
Kim Beomhee <nakaldi@gmail.com>
2025-05-21 23:39:44 +0900

    feat: Api 액세스토근과 접근 키 발급을 위한 ApiOAuthManager 클래스 작성, 빈 등록 코드 추가

--- src/main/java/io/cavia/mockinvest/config/SpringConfig.java
+++ src/main/java/io/cavia/mockinvest/config/SpringConfig.java
@@ -1,7 +1,21 @@
 package io.cavia.mockinvest.config;
 
+import io.cavia.mockinvest.client.ApiOAuthManager;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.reactive.function.client.WebClient;
 
 @Configuration
 public class SpringConfig {
+
+    private final WebClient.Builder webClientBuilder;
+
+    public SpringConfig(WebClient.Builder webClientBuilder) {
+        this.webClientBuilder = webClientBuilder;
+    }
+
+    @Bean
+    public ApiOAuthManager apiOAuthManager() {
+        return new ApiOAuthManager(webClientBuilder);
+    }
 }

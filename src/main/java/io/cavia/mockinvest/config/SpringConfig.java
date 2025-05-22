7f46332bf5e95c5b0c07ff000ecf30c4ee0f7e60
xoz0359 <k84890690@gmail.com>
2025-05-22 11:19:26 +0900

    feat: config계층에 mapper를 bean으로 등록했습니다.

--- src/main/java/io/cavia/mockinvest/config/SpringConfig.java
+++ src/main/java/io/cavia/mockinvest/config/SpringConfig.java
@@ -1,7 +1,25 @@
 package io.cavia.mockinvest.config;
 
+import io.cavia.mockinvest.dto.response.KorStockRealTimeDto;
+import io.cavia.mockinvest.mapper.KorOrderRealTimeMapper;
+import io.cavia.mockinvest.mapper.KorStockRealTimeMapper;
+import org.mapstruct.Mapper;
+import org.mapstruct.factory.Mappers;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class SpringConfig {
+
+
+    @Bean
+    public KorStockRealTimeMapper korStockRealTimeMapper() {
+        return Mappers.getMapper(KorStockRealTimeMapper.class);
+    }
+
+    @Bean
+    public KorOrderRealTimeMapper korOrderRealTimeMapper() {
+        return Mappers.getMapper(KorOrderRealTimeMapper.class);
+    }
+
 }

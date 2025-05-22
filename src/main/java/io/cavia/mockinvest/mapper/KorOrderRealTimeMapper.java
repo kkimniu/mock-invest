50f96fb22adb32359abbf468e459b077d5b4cad4
xoz0359 <k84890690@gmail.com>
2025-05-22 15:18:13 +0900

    feat: 각 필드의 항목에 맞게 dvs를 파싱했습니다.

--- src/main/java/io/cavia/mockinvest/mapper/KorOrderRealTimeMapper.java
+++ src/main/java/io/cavia/mockinvest/mapper/KorOrderRealTimeMapper.java
@@ -2,6 +2,8 @@ package io.cavia.mockinvest.mapper;
 
 import io.cavia.mockinvest.domain.OrderRealTime;
 
+import java.math.BigDecimal;
+
 public class KorOrderRealTimeMapper {
     /**
      * 국내주식 실시간호가 요청으로 발생하는 dvs를 Entity로 변환하는 메서드 입니다
@@ -20,61 +22,61 @@ public class KorOrderRealTimeMapper {
             datas[0],   // mkscShrnIscd
             datas[1],   // bsopHour
             datas[2],   // hourClsCode
-            datas[3],   // askp1
-            datas[4],   // askp2
-            datas[5],   // askp3
-            datas[6],   // askp4
-            datas[7],   // askp5
-            datas[8],   // askp6
-            datas[9],   // askp7
-            datas[10],  // askp8
-            datas[11],  // askp9
-            datas[12],  // askp10
-            datas[13],  // bidp1
-            datas[14],  // bidp2
-            datas[15],  // bidp3
-            datas[16],  // bidp4
-            datas[17],  // bidp5
-            datas[18],  // bidp6
-            datas[19],  // bidp7
-            datas[20],  // bidp8
-            datas[21],  // bidp9
-            datas[22],  // bidp10
-            datas[23],  // askpRsqn1
-            datas[24],  // askpRsqn2
-            datas[25],  // askpRsqn3
-            datas[26],  // askpRsqn4
-            datas[27],  // askpRsqn5
-            datas[28],  // askpRsqn6
-            datas[29],  // askpRsqn7
-            datas[30],  // askpRsqn8
-            datas[31],  // askpRsqn9
-            datas[32],  // askpRsqn10
-            datas[33],  // bidpRsqn1
-            datas[34],  // bidpRsqn2
-            datas[35],  // bidpRsqn3
-            datas[36],  // bidpRsqn4
-            datas[37],  // bidpRsqn5
-            datas[38],  // bidpRsqn6
-            datas[39],  // bidpRsqn7
-            datas[40],  // bidpRsqn8
-            datas[41],  // bidpRsqn9
-            datas[42],  // bidpRsqn10
-            datas[43],  // totalAskpRsqn
-            datas[44],  // totalBidpRsqn
-            datas[45],  // ovtmTotalAskpRsqn
-            datas[46],  // ovtmTotalBidpRsqn
-            datas[47],  // antcCnpr
-            datas[48],  // antcCnqn
-            datas[49],  // antcVol
-            datas[50],  // antcCntgVrss
+            Integer.parseInt(datas[3]),   // askp1
+            Integer.parseInt(datas[4]),   // askp2
+            Integer.parseInt(datas[5]),   // askp3
+            Integer.parseInt(datas[6]),   // askp4
+            Integer.parseInt(datas[7]),   // askp5
+            Integer.parseInt(datas[8]),   // askp6
+            Integer.parseInt(datas[9]),   // askp7
+            Integer.parseInt(datas[10]),  // askp8
+            Integer.parseInt(datas[11]),  // askp9
+            Integer.parseInt(datas[12]),  // askp10
+            Integer.parseInt(datas[13]),  // bidp1
+            Integer.parseInt(datas[14]),  // bidp2
+            Integer.parseInt(datas[15]),  // bidp3
+            Integer.parseInt(datas[16]),  // bidp4
+            Integer.parseInt(datas[17]),  // bidp5
+            Integer.parseInt(datas[18]),  // bidp6
+            Integer.parseInt(datas[19]),  // bidp7
+            Integer.parseInt(datas[20]),  // bidp8
+            Integer.parseInt(datas[21]),  // bidp9
+            Integer.parseInt(datas[22]),  // bidp10
+            Long.parseLong(datas[23]),  // askpRsqn1
+            Long.parseLong(datas[24]),  // askpRsqn2
+            Long.parseLong(datas[25]),  // askpRsqn3
+            Long.parseLong(datas[26]),  // askpRsqn4
+            Long.parseLong(datas[27]),  // askpRsqn5
+            Long.parseLong(datas[28]),  // askpRsqn6
+            Long.parseLong(datas[29]),  // askpRsqn7
+            Long.parseLong(datas[30]),  // askpRsqn8
+            Long.parseLong(datas[31]),  // askpRsqn9
+            Long.parseLong(datas[32]),  // askpRsqn10
+            Long.parseLong(datas[33]),  // bidpRsqn1
+            Long.parseLong(datas[34]),  // bidpRsqn2
+            Long.parseLong(datas[35]),  // bidpRsqn3
+            Long.parseLong(datas[36]),  // bidpRsqn4
+            Long.parseLong(datas[37]),  // bidpRsqn5
+            Long.parseLong(datas[38]),  // bidpRsqn6
+            Long.parseLong(datas[39]),  // bidpRsqn7
+            Long.parseLong(datas[40]),  // bidpRsqn8
+            Long.parseLong(datas[41]),  // bidpRsqn9
+            Long.parseLong(datas[42]),  // bidpRsqn10
+            Long.parseLong(datas[43]),  // totalAskpRsqn
+            Long.parseLong(datas[44]),  // totalBidpRsqn
+            Long.parseLong(datas[45]),  // ovtmTotalAskpRsqn
+            Long.parseLong(datas[46]),  // ovtmTotalBidpRsqn
+            Integer.parseInt(datas[47]),  // antcCnpr
+            Long.parseLong(datas[48]),  // antcCnqn
+            Long.parseLong(datas[49]),  // antcVol
+            Integer.parseInt(datas[50]),  // antcCntgVrss
             datas[51],  // antcCntgVrssSign
-            datas[52],  // antcCntgPrdyCtrt
-            datas[53],  // acmlVol
-            datas[54],  // totalAskpRsqnIcdc
-            datas[55],  // totalBidpRsqnIcdc
-            datas[56],  // ovtmTotalAskpIcdc
-            datas[57]   // ovtmTotalBidpIcdc
+            new BigDecimal(datas[52]),  // antcCntgPrdyCtrt
+            Long.parseLong(datas[53]),  // acmlVol
+            Integer.parseInt(datas[54]),  // totalAskpRsqnIcdc
+            Integer.parseInt(datas[55]),  // totalBidpRsqnIcdc
+            Integer.parseInt(datas[56]),  // ovtmTotalAskpIcdc
+            Integer.parseInt(datas[57])   // ovtmTotalBidpIcdc
         );
     }
 }
